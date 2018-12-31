48fc82d64642a26656f68f4f0f628504ae08e479
Shinmera <shinmera@tymoon.eu>
2018-12-31 09:27:41 +0100

    Doc up

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -1,1 +1,5 @@
 (asdf:load-system :staple-markless)
+
+(defmethod staple:subsystems ((system (eql (asdf:find-system :cl-markless))))
+  (list (asdf:find-system :cl-markless-plump)
+        (asdf:find-system :cl-markless-standalone)))
